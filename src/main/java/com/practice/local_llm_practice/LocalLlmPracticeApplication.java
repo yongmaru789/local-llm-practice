0321acbf6effafb43130102a759f697f80f2a111
JAEUNGCHOI <jaeung0129@gmail.com>
2026-09-13 15:47:35 +0900

    Ollama 로컬 환경과 Spring Boot 프로젝트 뼈대를 만든다

---
+++ src/main/java/com/practice/local_llm_practice/LocalLlmPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.practice.local_llm_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalLlmPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalLlmPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practice/local_llm_practice/LocalLlmPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.local_llm_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalLlmPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
