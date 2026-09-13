8adde26ffcb54138aec9ae33ad97a712434ab10c
JAEUNGCHOI <jaeung0129@gmail.com>
2026-09-13 18:07:40 +0900

    Ollama 서비스를 테스트할 API 엔드포인트를 추가한다

--- src/main/java/com/practice/local_llm_practice/TestController.java
+++ src/main/java/com/practice/local_llm_practice/TestController.java
@@ -1,4 +1,20 @@
 package com.practice.local_llm_practice;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class TestController {
+
+    private final OllamaService ollamaService;
+
+    public TestController(OllamaService ollamaService) {
+        this.ollamaService = ollamaService;
+    }
+
+    @GetMapping("/test/ollama")
+    public String testOllama() {
+        return ollamaService.chat("안녕");
+    }
+
 }
